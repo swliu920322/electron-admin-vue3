c0718f7133d7f4ca41a7cfe5b69454f876554431
liushengwei <1064084779@qq.com>
2024-08-02 22:59:33 +0800

    feat: make a tag

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,14 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { devPlugin } from './plugins/devPlugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    devPlugin(),
     vue(),
-    vueDevTools(),
+    vueDevTools()
   ],
   resolve: {
     alias: {
